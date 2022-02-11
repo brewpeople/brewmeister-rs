766bf357c8e49c2c51ad3a9ec4824c7ccfda5beb
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2022-02-11 21:11:48 +0100

    api: simulate mock change with smooth roll-off

--- api/src/devices/mock.rs
+++ api/src/devices/mock.rs
@@ -1,15 +1,29 @@
 use crate::devices::Device;
 use crate::AppError;
+use std::time::Instant;
 use tracing::instrument;
 
 #[derive(Debug)]
 pub struct Mock {
-    temperature: f32,
+    target_temperature: f32,
+    last_time: Instant,
+    last_temperature: f32,
 }
 
 impl Mock {
     pub fn new() -> Self {
-        Self { temperature: 20.0 }
+        Self {
+            target_temperature: 20.0,
+            last_time: Instant::now(),
+            last_temperature: 19.0,
+        }
+    }
+
+    fn current_temperature(&self) -> f32 {
+        let duration = Instant::now() - self.last_time;
+        let delta_temp = self.target_temperature - self.last_temperature;
+        let y = (duration.as_secs_f32() / 5.0).tanh();
+        self.last_temperature + y * delta_temp
     }
 }
 
@@ -18,8 +32,8 @@ impl Device for Mock {
     #[instrument]
     async fn read(&self) -> Result<models::Device, AppError> {
         Ok(models::Device {
-            current_temperature: Some(self.temperature),
-            target_temperature: Some(self.temperature),
+            current_temperature: Some(self.current_temperature()),
+            target_temperature: Some(self.target_temperature),
             stirrer_on: false,
             heater_on: false,
             serial_problem: false,
@@ -28,7 +42,9 @@ impl Device for Mock {
 
     #[instrument]
     async fn set_temperature(&mut self, temperature: f32) -> Result<(), AppError> {
-        self.temperature = temperature;
+        self.last_time = Instant::now();
+        self.last_temperature = self.current_temperature();
+        self.target_temperature = temperature;
         Ok(())
     }
 }
